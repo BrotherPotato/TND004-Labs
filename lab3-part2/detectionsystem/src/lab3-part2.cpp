c00d7fd52d1023e6bfb1aa51a9406a8937f6d025
Max Wiklundh <wiklundhmax@gmail.com>
2024-04-24 16:04:42 +0200

    wow

--- lab3-part2/detectionsystem/src/lab3-part2.cpp
+++ lab3-part2/detectionsystem/src/lab3-part2.cpp
@@ -17,29 +17,38 @@ struct Point {
     double y = 0.0;
 };
 
-struct Line {
-    Point start = Point{};
-    Point end = Point{};
-
+struct LineSegment {
+    Point start = Point{0, 0};
+    Point end = Point{0, 0};
     double slope = 0.0;
 };
 
+struct CompleteLine {
+    //Point start = Point{0, 0};
+    //Point end = Point{0, 0};
+    std::vector<Point> intermediaryPoints = {};
+    double slope = 0.0;
+};
 
+//std::vector<std::vector<Point>>
 
 class Lines {
 public:
     Lines();
     ~Lines() = default;
 
-    void calcSlope(Line&);
+    void calcSlope(LineSegment&);
 
 private:
-    std::vector<Line> lineVec;
+    std::vector<LineSegment> lineVec;
 };
 
 Lines::Lines() {}
 
-void Lines::calcSlope(Line& L) { L.slope = (L.end.y - L.start.y) / (L.end.x - L.start.x); } //vill vi att den beräknar för en eller för alla och ska den returnera något eller vill vi ta byrefrence
+void Lines::calcSlope(LineSegment& L) {
+    L.slope = (L.end.y - L.start.y) / (L.end.x - L.start.x);
+}  // vill vi att den beräknar för en eller för alla och ska den returnera något eller vill vi ta
+   // byrefrence
 
 std::vector<Point> fileReader(const std::string& filename) {
     std::filesystem::path points_name = filename;
@@ -63,7 +72,26 @@ std::vector<Point> fileReader(const std::string& filename) {
         p.x /= 32767.0;
         p.y /= 32767.0;
     }
+}
 
+void writeFile(std::vector<CompleteLine>& lines, std::string readFileName) {
+    std::ofstream out_file("../data/segments-" + readFileName + ".txt");
+    if (!out_file) {
+        std::cout << "Error opening output file!!\n";
+        return;
+    }
+    //out_file << lines.size() << '\n';
+    for (const auto& line : lines) {
+        if (line.intermediaryPoints.size() < 3) {
+            continue;
+        }
+        //out_file << "(" << line.start.x << "," << line.start.y << ")";
+        for (Point currentPoint : line.intermediaryPoints) {
+            out_file << "(" << currentPoint.x << "," << currentPoint.y << ")->";
+        }
+        out_file << '\n';
+    }
+    out_file.close();
 }
 
 int main() try {
@@ -74,6 +102,12 @@ int main() try {
     std::vector<Point> allPoints = fileReader(s);
 
     std::cout << "A";
+
+    std::vector<LineSegment> allLines; 
+
+    std::vector<CompleteLine> allCompleteLines;
+
+    writeFile(allCompleteLines, s);
 
     plotData(s);
 } catch (const std::exception& e) {
